519e65787d514eedbdeb85bf6f2440351caf75bb
Rakiel <137511589+MthsSantos@users.noreply.github.com>
2026-06-16 23:51:01 -0300

    Adicionando Arquivos de View
    Arquivos conferidos em conjunto

--- AV2/Biblioteca/src/view/AbaEmprestimo.java
+++ AV2/Biblioteca/src/view/AbaEmprestimo.java
@@ -33,11 +33,14 @@ public class AbaEmprestimo extends JPanel {
 	private boolean existeCliente;
 	private Cliente cliente;
 
-	
+	//Remove todos os caracteres que não são números do CPF 
+	// XXX.XXX.XXX-XX = XXXXXXXXXXX / XXX XXX XXX XX = XXXXXXXXXXX
 	private String limparCpf(String cpf) {
 	    return cpf.replaceAll("[^0-9]", "");
 	}
 
+    //Método responsável por formatar o CPF
+    //XXXXXXXXXXX vira XXX.XXX.XXX-XX
 	private String formatarCpf(String cpf) {
 	    return cpf.substring(0, 3) + "." +
 	           cpf.substring(3, 6) + "." +
@@ -45,19 +48,21 @@ public class AbaEmprestimo extends JPanel {
 	           cpf.substring(9, 11);
 	}
 	
+    //Método responsável por remover tudo que não for número do telefone
+    // (21) 9XXXX-XXXX = 219XXXXXXXX / 21 9XXXXXXXX = 219XXXXXXXX 
 	private String limparTelefone(String telefone) {
 	    return telefone.replaceAll("[^0-9]", "");
 	}
 
+    //Método responsável por formatar o telefone
+    //11987654321 vira (11) 98765-4321
 	private String formatarTelefone(String telefone) {
 	    return "(" + telefone.substring(0, 2) + ") " +
 	           telefone.substring(2, 7) + "-" +
 	           telefone.substring(7, 11);
 	}
 	
-	/**
-	 * Create the panel.
-	 */
+
 	public AbaEmprestimo() {
 		
 		JLabel lblNewLabel = new JLabel("Emprestimo");
@@ -78,10 +83,14 @@ public class AbaEmprestimo extends JPanel {
 		btnBuscar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				//Pega o cpf digitado e remove os espaços extras com o .trim()
 				String pesquisaCpf = tfPesquisarCpf.getText().trim();
 
+				//Cria uma variavel que chama o método para remover o que não for número
 				String cpfLimpo = limparCpf(pesquisaCpf);
 
+				//Validação para o cpf conter 11 numeros
 				if (cpfLimpo.length() != 11) {
 				    JOptionPane.showMessageDialog(
 				        null,
@@ -90,12 +99,13 @@ public class AbaEmprestimo extends JPanel {
 				    return;
 				}
 
+				//Formata o cpf que foi limpo e validado
 				String cpfFormatado = formatarCpf(cpfLimpo);
 
-				// Mostra formatado no campo de pesquisa
+				//Mostra formatado no campo de pesquisa
 				tfPesquisarCpf.setText(cpfFormatado);
 
-				// Use este se você quer salvar/buscar CPF com ponto e traço no banco
+				//Criar um objeto cliente com o cpfFormatado para preencher o DAO
 				cliente = new Cliente(cpfFormatado);
 			    
 			    ConexaoBD bd = new ConexaoBD();
@@ -103,29 +113,40 @@ public class AbaEmprestimo extends JPanel {
 			    if (bd.connect()) {
 			    	ClienteDAO clienteDao = new ClienteDAO(bd, cliente);
 
+			    	//Busca Cliente
 			    	boolean estado = clienteDao.buscar();
 			        
 			    	bd.close();
 
+			    	//Se encontrado
 			    	if (estado) {
+			    		
+			    		//Marca que o cliente já existe no banco
 			    		existeCliente = true;
 			    		
+			    		//Preenche os campos com os dados do cliente encontrado
 			    		tfCpf.setText(cliente.getCpf());
 			    		tfNome.setText(cliente.getNome());
 			    		tfTelefone.setText(cliente.getTelefone());
 			    		tfEmail.setText(cliente.getEmail());
 			    	} else {
+			    		
+			    		//Marca que o cliente ainda não existe no banco
 			    		existeCliente = false;
 			   
+			    		//Printa mensagem avisando ao usuario
 			    		JOptionPane.showMessageDialog(null, "Cliente não cadastrado.");
 
-			    		tfCpf.setText(cpfFormatado);; // preenche automaticamente o CPF digitado
+			    		//Preenche automaticamente o CPF digitado 
+			    		tfCpf.setText(cpfFormatado);
 			    		
+			    		//Limpa os outros campos
 			    		tfNome.setText("");
 			    		tfTelefone.setText("");
 			    		tfEmail.setText("");
 			    		
-			    	    tfNome.requestFocus(); //posiciona o cursor no nome
+			    		//Posiciona o cursor no campo nome
+			    	    tfNome.requestFocus(); 
 			    	}	    		        
 			        
 			    } else {
@@ -200,21 +221,37 @@ public class AbaEmprestimo extends JPanel {
 		btnNewButton.setFont(new Font("Arial", Font.BOLD, 10));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				//Pega o id do livro
 		        String idLivroTexto = textField.getText();
+		        
+		        //Pega o cpf e remove espaços extras com o .trim()
 		        String cpfDigitado = tfCpf.getText().trim();
+		        
+		        //Remove o que não for número para o cpf
 		        String cpfLimpo = limparCpf(cpfDigitado);
 
+		        
+		        //Válidação 
 		        if (cpfLimpo.length() != 11) {
 		            JOptionPane.showMessageDialog(null, "CPF inválido. Digite exatamente 11 números.");
 		            return;
 		        }
 
+		        //Formata o cpf
 		        String cpf = formatarCpf(cpfLimpo);
 		        tfCpf.setText(cpf);
+		        
+		        //Pega nome digitado
 		        String nome = tfNome.getText();
+		        
+		        //Pega o telefone digitado e tira os espaços extras
 		        String telefoneDigitado = tfTelefone.getText().trim();
+		        
+		        //Tira tudo que não for numero
 		        String telefoneLimpo = limparTelefone(telefoneDigitado);
 
+		        //Válidação para confirmar tamanho do telefone
 		        if (telefoneLimpo.length() != 11) {
 		            JOptionPane.showMessageDialog(
 		                null,
@@ -223,33 +260,44 @@ public class AbaEmprestimo extends JPanel {
 		            return;
 		        }
 
+		        //Formata o telefone validado
 		        String telefone = formatarTelefone(telefoneLimpo);
 		        tfTelefone.setText(telefone);
+		       
+		        //Pega o email digitado
 		        String email = tfEmail.getText();
 
+		        //Se algum campo estiver vazio printa na tela
 		        if (idLivroTexto.isEmpty() || cpf.isEmpty() || nome.isEmpty()
 		                || telefone.isEmpty() || email.isEmpty()) {
 		            JOptionPane.showMessageDialog(null, "Preencha todos os campos.");
 		            return;
 		        }
 
+		        //Variável que receberá o ID do livro convertido
 		        int idLivro;
 
 		        try {
+		        	//Converte o id para numero inteiro
 		            idLivro = Integer.parseInt(idLivroTexto);
 		        } catch (NumberFormatException erro) {
+		        	
+		        	// Caso não seja número, exibe mensagem de erro
 		            JOptionPane.showMessageDialog(null, "O ID do livro deve ser um número.");
 		            return;
 		        }
 
+		        //Cria o cliente com CPF e dados informados
 		        Cliente cliente = new Cliente(cpf);
 		        cliente.setNome(nome);
 		        cliente.setTelefone(telefone);
 		        cliente.setEmail(email);
 
+		        //Cria o livro apenas com o id
 		        Livro livro = new Livro();
 		        livro.setIdLivro(idLivro);
 
+		        //Cria um emprestimo definindo o livro e o cliente
 		        Emprestimo emprestimo = new Emprestimo();
 		        emprestimo.setCliente(cliente);
 		        emprestimo.setLivro(livro);
@@ -258,19 +306,26 @@ public class AbaEmprestimo extends JPanel {
 
 		        if (bd.connect()) {
 
+		        	//Se o cliente não existir, cadastra ele primeiro
 		            if (!existeCliente) {
 		                ClienteDAO clienteDAO = new ClienteDAO(bd, cliente);
+		                
+		                //Cadastra o cliente no banco de dados
 		                String msgCliente = clienteDAO.atualizar(TipoAtualizaBD.Criar);
+		                
 		                JOptionPane.showMessageDialog(null, msgCliente);
 		            }
 
 		            EmprestimoDAO emprestimoDAO = new EmprestimoDAO(bd, emprestimo);
+		            
+		            //Cria o emprestimo no banco de dados
 		            String msgEmprestimo = emprestimoDAO.atualizar(TipoAtualizaBD.Criar);
 
 		            JOptionPane.showMessageDialog(null, msgEmprestimo);
 
 		            bd.close();
 
+		            //Limpa os campos da tela pós criação do emprestimo 
 		            textField.setText("");
 		            tfPesquisarCpf.setText("");
 		            tfCpf.setText("");
@@ -290,11 +345,14 @@ public class AbaEmprestimo extends JPanel {
 		btnEmprestimos.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				//Verifica se existe o cliente pesquisado
 				if (cliente == null) {
 				    JOptionPane.showMessageDialog(null, "Pesquise um cliente primeiro.");
 				    return;
 				}
 
+				//Abre a tela de emprestimos ativos do cliente pesquisado
 				EmprestimoAtivo cadastroAtivo = new EmprestimoAtivo(cliente);
 				cadastroAtivo.setVisible(true);
 			}
